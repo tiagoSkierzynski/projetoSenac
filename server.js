8490022e5ba50a696e442502e5540f307da88ae1
Tiago <tiagoskierzynski@gmail.com>
2020-12-17 21:36:14 -0300

    atualização aula 17/12/2020, quinta-feira

--- server.js
+++ server.js
@@ -3,6 +3,19 @@ const usuario = require("./models/usuario")
 const express = require("express")
 const app = express()
 
+const handlebars = require("express-handlebars")
+const bodyParser = require("body-parser")
+
+app.engine('handlebars',handlebars({defaultLayout:'main'}))
+app.set('view engine','handlebars')
+
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+app.use('/static', express .static(__dirname + '/public'));
+
+
+
 app.get('/delete/:id',function(req,res){
     usuario.destroy({
         where:{'id': req.params.id}
@@ -19,25 +32,11 @@ app.get('/delete/:id',function(req,res){
 
 //VAMOS CRIAR MAIS UMA ROTA E ELA DARA PARA UM FORMULÁRIO
 app.get('/update/:id',function(req,res){
-    usuario.findAll({ where:{id : req.params.id}}).then(function(doadores){
+    usuario.findAll({ where:{'id' : req.params.id}}).then(function(doadores){
         res.render('atualiza',{doador: doadores.map(pagamento => pagamento.toJSON())})
     })
 })
 
-//DEPOIS VAMOS CRIAR ESSA ROTA QUE ENVIA PARA O BANCO E DEPOIS CHAMA O FORMULARIO
-app.post('/updateUsuario',function(req,res){
-    usuario.update({Nome:req.body.nome,senha:req.body.senha },{
-        where:{id:req.body.codigo}}
-    ).then(function(){
-        usuario.findAll().then(function(doadores){
-            res.render('formulario',{doador: doadores.map(pagamento => pagamento.toJSON())})
-        })
-    }).catch(function(erro){
-        res.send("erro "+erro)
-    })
-})
-
-app.listen(3000);
 
 app.get('/formulario',function(req,res){
     usuario.findAll().then(function(doadores){
@@ -45,16 +44,7 @@ app.get('/formulario',function(req,res){
     })
 })
 
-const handlebars = require("express-handlebars")
-const bodyParser = require("body-parser")
 
-app.engine('handlebars',handlebars({defaultLayout:'main'}))
-app.set('view engine','handlebars')
-
-app.use(bodyParser.urlencoded({extended:false}))
-app.use(bodyParser.json())
-
-app.use('/static', express .static(__dirname + '/public'));
 
 //esse bloco é disparado pelo enviar do formulario
 app.post('/cadUsuario',function(req,res){
@@ -65,7 +55,6 @@ app.post('/cadUsuario',function(req,res){
         usuario.findAll().then(function(doadores){
             res.render('formulario',{doador: doadores.map(pagamento => pagamento.toJSON())})
         })
-        res.render("formulario")
     }).catch(function(erro){
         res.send("Erro "+erro)
     })
@@ -94,3 +83,19 @@ app.get("/login", function(req,res){
 app.get("/cadastro", function(req,res){
     res.render("cadastro")
 })
+
+
+//DEPOIS VAMOS CRIAR ESSA ROTA QUE ENVIA PARA O BANCO E DEPOIS CHAMA O FORMULARIO
+app.post('/updateUsuario',function(req,res){
+    usuario.update({nome:req.body.nome,senha:req.body.senha },{
+        where:{id:req.body.codigo}}
+    ).then(function(){
+        usuario.findAll().then(function(doadores){
+            res.render('formulario',{doador: doadores.map(pagamento => pagamento.toJSON())})
+    })
+    }).catch(function(erro){
+        res.send("Erro "+erro)
+    })
+})
+
+app.listen(3000);
