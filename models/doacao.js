3cd56f0ff62d99b064fd471c8a51259f2a1a8562
Tiago Skierzynski <tiagoskierzynski@gmail.com>
2021-03-15 19:56:40 -0300

    versão atualizada 15/03/2021

---
+++ models/doacao.js
@@ -0,0 +1,22 @@
+const db = require('./db')
+
+//Criando tabela de cadastro de ONG no banco de dados
+const doacaoCadastro = db.sequelize.define('doacaocadastros',{
+   categoria:{
+       type:db.Sequelize.STRING
+   },descricao:{
+    type:db.Sequelize.STRING
+},nivel:{
+    type:db.Sequelize.STRING
+},idOng:{
+    type:db.Sequelize.INTEGER
+},
+})
+
+
+//Cria tabela - somente uma vez
+//doacaoCadastro.sync({force:true})
+
+
+//exportando a constante
+module.exports= doacaoCadastro
